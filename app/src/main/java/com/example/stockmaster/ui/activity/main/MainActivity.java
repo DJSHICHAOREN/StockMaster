675f53747a3719a54a02e1fe0d331dc70cb28f2b
lvwuda <lvwuda@dj.com>
2020-04-02 16:55:48 +0800

    添加手机通知功能

--- app/src/main/java/com/example/stockmaster/ui/activity/main/MainActivity.java
+++ app/src/main/java/com/example/stockmaster/ui/activity/main/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.stockmaster.ui.activity.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.Vibrator;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -67,6 +77,11 @@ public class MainActivity extends AppCompatActivity {
 //        bindService(intent, serviceConnection, BIND_AUTO_CREATE);
 
         mMainPresent.beginQueryAndAnalyse();
+
+        // 设置手机震动
+        Vibrator v = (Vibrator) MainActivity.this.getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        v.vibrate(500);
     }
 
     public void notifyStockListDataSetChanged(){
@@ -79,11 +94,66 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public class MainActivityUIManager {
+        private String CHANNEL_ID = "STOCK_MASTER_CHANNEL";
+        private int notificationId = 0;
+        public MainActivityUIManager(){
+            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            createNotificationChannel();
+        }
+
         public void refreshUIWhenReceiveNewPrice(Stock stock){
         }
 
+        /**
+         * 创建通知频道
+         */
+        private void createNotificationChannel() {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                CharSequence name = getString(R.string.channel_name);
+                String description = getString(R.string.channel_description);
+                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+                channel.setDescription(description);
+                // Register the channel with the system; you can't change the importance
+                // or other notification behaviors after this
+                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        /**
+         * 发送notification
+         * @param notificationContent 发送的内容
+         */
+        public void sendNotification(String notificationContent){
+            // Create an explicit intent for an Activity in your app
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle("交易通知")
+                    .setVibrate(new long[]{0, 1000,1000,1000})
+                    .setContentText(notificationContent)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
+            // notificationId is a unique int for each notification that you must define
+            notificationManager.notify(notificationId++, builder.build());
+        }
+
+        /**
+         * 处理买卖点
+         * @param dealString
+         */
         public void refreshUIWhenGetNewDealPoint(String dealString) {
             tv_deal_point.setText(dealString);
+//            sendNotification(dealString);
         }
     }
 
